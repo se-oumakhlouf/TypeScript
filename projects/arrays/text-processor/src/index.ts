832e41498801f03d7d5e094afd700c171f78f536
Selym OUMAKHLOUF <s.oumakhlouf@actongroup.com>
2025-05-14 10:39:16 +0200

    array text processor

--- projects/arrays/text-processor/src/index.ts
+++ projects/arrays/text-processor/src/index.ts
@@ -1,2 +1,71 @@
-// Write your alignTexts function here! ✨
-// You'll need to export it so the tests can run it.
+export type AlignmentOptions = {
+	align?: "left" | "middle" | "right";
+	width: number;
+};
+
+export function alignTexts(texts: string[], options: AlignmentOptions) {
+	const resultLines: string[][] = [];
+
+	for (const text of texts) {
+		const lines: string[] = splitLines(text, options.width);
+		const align: string[] = alignLines(lines, options);
+		resultLines.push(align);
+	}
+	return resultLines;
+}
+
+function splitLines(text: string, width: number): string[] {
+	const result: string[] = [];
+
+	let chain: string = "";
+	const splitText = text.split(" ");
+	let separator = "";
+	let isModified: boolean = false;
+
+	for (const word of splitText) {
+		if (isModified && chain.length + word.length + separator.length > width) {
+			result.push(chain);
+			chain = "";
+			separator = "";
+			isModified = false;
+		}
+		chain += separator + word;
+		isModified = true;
+		if (separator === "") {
+			separator += " ";
+		}
+	}
+	if (isModified) {
+		result.push(chain);
+	}
+	return result;
+}
+
+function alignLines(
+	lines: string[],
+	{ align = "left", width }: AlignmentOptions,
+): string[] {
+	const result: string[] = [];
+	for (let line of lines) {
+		if (line.length === width) {
+			result.push(line);
+			continue;
+		}
+		switch (align) {
+			case "left":
+				line = line.padEnd(width, " ");
+				break;
+			case "right":
+				line = line.padStart(width, " ");
+				break;
+			case "middle":
+				const remaining = width - line.length;
+				const startPad = Math.floor(remaining / 2);
+				const endPad = startPad + (remaining % 2);
+				line = " ".repeat(startPad) + line + " ".repeat(endPad);
+				break;
+		}
+		result.push(line);
+	}
+	return result;
+}
