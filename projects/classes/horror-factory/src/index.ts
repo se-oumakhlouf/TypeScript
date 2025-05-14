8e2c64c241e016e12456d49d8ff88fdd0e89d873
Selym OUMAKHLOUF <s.oumakhlouf@actongroup.com>
2025-05-14 14:31:38 +0200

    classes - horror basic + factory -> (factory from solution.ts)

--- projects/classes/horror-factory/src/index.ts
+++ projects/classes/horror-factory/src/index.ts
@@ -1,2 +1,68 @@
-// Write your class and functions here! ✨
-// You'll need to export the class and functions so the tests can run it.
+interface Consumed {
+	evil: boolean;
+	name: string;
+	power: number;
+}
+
+interface HorrorSettings {
+	name: string;
+	getPowerFrom: (consumed: Consumed) => number;
+	isEvil: () => boolean;
+}
+
+export class Horror {
+	#consumed: Consumed[] = [];
+
+	readonly isEvil: () => boolean;
+	readonly name: string;
+	readonly #getPowerFrom: (consumed: Consumed) => number;
+
+	constructor({ name, getPowerFrom, isEvil }: HorrorSettings) {
+		this.isEvil = isEvil;
+		this.name = name;
+		this.#getPowerFrom = getPowerFrom;
+	}
+
+	#consume(opponent: Horror) {
+		this.#consumed.push({
+			evil: opponent.isEvil(),
+			name: opponent.name,
+			power: opponent.getPower(),
+		});
+	}
+
+	getPower(): number {
+		let power = 0;
+		for (const entity of this.#consumed) {
+			power += this.#getPowerFrom(entity);
+		}
+		return power + this.#consumed.length;
+	}
+
+	doBattle(opponent: Horror) {
+		if (this.getPower() >= opponent.getPower()) {
+			this.#consume(opponent);
+		}
+	}
+}
+
+const demonSettings: HorrorSettings = {
+	name: "Demon",
+	getPowerFrom: (consumed: Consumed) => {
+		return consumed.evil ? consumed.power / 2 : consumed.power * 2;
+	},
+	isEvil: () => true,
+};
+
+export function createDemon() {
+	return new Horror(demonSettings);
+}
+
+export function createSorcerer(name: string, evil: boolean) {
+	return new Horror({
+		name,
+		getPowerFrom: (consumed: Consumed) =>
+			consumed.evil === evil ? consumed.power * 2 : consumed.power,
+		isEvil: () => evil,
+	});
+}
--- projects/classes/the-shape-of-types/src/index.ts
+++ projects/classes/the-shape-of-types/src/index.ts
@@ -1,2 +1,65 @@
-// Write your classes here! ✨
-// You'll need to export them so the tests can run them.
+interface Consumed {
+	evil: boolean;
+	name: string;
+	power: number;
+}
+
+export abstract class Horror {
+	abstract readonly name: string;
+	#consumed: Consumed[] = [];
+
+	#consume(opponent: Horror) {
+		this.#consumed.push({
+			evil: opponent.isEvil(),
+			name: opponent.name,
+			power: opponent.getPower(),
+		});
+	}
+
+	getPower(): number {
+		let power = 0;
+		for (const entity of this.#consumed) {
+			power += this.getPowerFrom(entity);
+		}
+		return power + this.#consumed.length;
+	}
+
+	doBattle(opponent: Horror) {
+		if (this.getPower() >= opponent.getPower()) {
+			this.#consume(opponent);
+		}
+	}
+
+	protected abstract getPowerFrom(consumed: Consumed): number;
+	protected abstract isEvil(): boolean;
+}
+
+export class Demon extends Horror {
+	name: string = "Demon";
+
+	protected getPowerFrom(consumed: Consumed): number {
+		return consumed.evil ? consumed.power / 2 : consumed.power * 2;
+	}
+	protected isEvil(): boolean {
+		return true;
+	}
+}
+
+export class Sorcerer extends Horror {
+	name: string;
+	#evil: boolean;
+
+	constructor(name: string, evil: boolean) {
+		super();
+		this.name = name;
+		this.#evil = evil;
+	}
+
+	getPowerFrom(consumed: Consumed): number {
+		return consumed.evil === this.#evil ? consumed.power * 2 : consumed.power;
+	}
+
+	isEvil(): boolean {
+		return this.#evil;
+	}
+}
