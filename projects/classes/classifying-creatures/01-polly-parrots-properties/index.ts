a78aeae81e9826adf4e380229ea0f8fb2a6da39c
Selym OUMAKHLOUF <s.oumakhlouf@actongroup.com>
2025-05-14 11:42:36 +0200

    classes - classifying creatures

--- projects/classes/classifying-creatures/01-polly-parrots-properties/index.ts
+++ projects/classes/classifying-creatures/01-polly-parrots-properties/index.ts
@@ -1,2 +1,21 @@
-// Write your Parrot class here! ✨
-// You'll need to export it so the tests can run it.
+export class Parrot {
+	#name: string;
+	#phrases: string[] = [];
+
+	constructor(name: string) {
+		this.#name = name;
+		this.#phrases.push(`${name} wants a cracker!`);
+	}
+
+	announce(): string {
+		return `Squawk! I'm ${this.#name}!`;
+	}
+
+	learn(phrase: string): void {
+		this.#phrases.push(phrase);
+	}
+
+	speak(): string {
+		return this.#phrases[Math.floor(Math.random() * this.#phrases.length)];
+	}
+}
--- projects/classes/classifying-creatures/03-hamster-helpers/index.ts
+++ projects/classes/classifying-creatures/03-hamster-helpers/index.ts
@@ -1,2 +1,50 @@
-// Write your type and classes here! ✨
-// You'll need to export the classes so the tests can run them.
+export type SmallPetFood =
+	| "bugs"
+	| "fruits"
+	| "insects"
+	| "plants"
+	| "seeds"
+	| "vegetables";
+
+export abstract class SmallFuryPet {
+	readonly species: string;
+	protected happiness: number = 0;
+
+	constructor(species: string) {
+		this.species = species;
+	}
+
+	abstract eats(food: SmallPetFood): boolean;
+
+	isHappy(): boolean {
+		return this.happiness > 0;
+	}
+}
+
+export class Gerbil extends SmallFuryPet {
+	constructor() {
+		super("Gerbil");
+	}
+
+	dig(): void {
+		this.happiness++;
+	}
+
+	eats(food: SmallPetFood): boolean {
+		return ["insects", "plants", "seeds", "vegetables"].includes(food);
+	}
+}
+
+export class Hamster extends SmallFuryPet {
+	constructor() {
+		super("Hamster");
+	}
+
+	run(): void {
+		this.happiness++;
+	}
+
+	eats(): boolean {
+		return true;
+	}
+}
